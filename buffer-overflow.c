8c995cfcb71f8c27c1d305e5db8134fd2e21309a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2021-02-02 10:35:26 -0700

    Update buffer-overflow.c

--- buffer-overflow.c
+++ buffer-overflow.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 void User_Input ()
+{
+  char day [8];
+  printf("Enter day:\n");
+  scanf("%s", day);
+  printf("It is %s", day);
+}
+
+{
+  int main ()
+  User_Input();
+  return 0
+}
